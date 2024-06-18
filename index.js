a6a321cb050ecdc2353cc204e819ed4f2ea0107d
Amin7775 <amin.dpi24@gmail.com>
2024-06-18 20:50:17 +0600

    Starting the server

---
+++ index.js
@@ -0,0 +1,19 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const port = process.env.PORT || 5000;
+
+
+// middleware
+app.use(cors())
+app.use(express.json())
+
+// root api
+app.get('/', (req,res)=>{
+    res.send("Tekup server is running")
+})
+
+app.listen(port,()=>{
+    console.log("Tekup server is running on port: ", port)
+})
