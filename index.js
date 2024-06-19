fc373511a9ef57ce3ad5b7c69ff9c5462e16b454
Amin7775 <amin.dpi24@gmail.com>
2024-06-19 21:46:41 +0600

    Installed mongodb and connected to db using environment variables

--- index.js
+++ index.js
@@ -9,6 +9,37 @@ const port = process.env.PORT || 5000;
 app.use(cors())
 app.use(express.json())
 
+
+// db
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_User}:${process.env.DB_Password}@clusterpherob9.3leb5bl.mongodb.net/?retryWrites=true&w=majority&appName=ClusterPheroB9`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // collections
+    const database = client.db("tekup");
+    const userCollection = database.collection('users')
+
+    
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
 // root api
 app.get('/', (req,res)=>{
     res.send("Tekup server is running")
